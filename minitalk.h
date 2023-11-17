d6065839dc11eab96de3ff25eb0e29cf854658c2
Izabel Zanoni <113716721+idzanoni@users.noreply.github.com>
2023-11-17 20:39:07 -0300

    Add files via upload

---
+++ minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: izanoni <izanoni@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/23 16:23:46 by izanoni           #+#    #+#             */
+/*   Updated: 2023/10/30 19:36:05 by izanoni          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <unistd.h>
+# include <stdio.h>
+
+# include "../libft/libft.h"
+# include "../ft_printf/ft_printf.h"
+
+#endif
